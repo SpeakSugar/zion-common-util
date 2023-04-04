8d3d7cc4adbc247fd1b642d1b1a061e69df16212
jeffries.yu <jeffries.yu@ringcentral.com>
2023-04-04 11:00:41 +0800

    feat: add func to StringUtil

--- src/string-util.ts
+++ src/string-util.ts
@@ -9,13 +9,21 @@ export class StringUtil {
         return origin.substring(0, origin.lastIndexOf(char));
     }
 
+    public static substringToFirstChar(origin: string, char: string) {
+        return origin.substring(0, origin.indexOf(char));
+    }
+
     /**
      * Example: "chromedriver" = StringUtil.substringFromLastChar("./drivers/win/chromedriver", "/")
      * @param origin
      * @param char
      */
-    public static substringFromLastChar(origin: string, char: string) {
+    public static substringFromLastChar(origin: string, char: string): string {
         return origin.substring(origin.lastIndexOf(char) + 1, origin.length);
+    }
+
+    public static substringFromFirstChar(origin: string, char: string): string {
+        return origin.substring(origin.indexOf(char) + 1, origin.length);
     }
 
 }
