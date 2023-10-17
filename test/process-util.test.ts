89af2ec3a0b9c61854a52bd3b765e1b94027c035
jeffries.yu <jeffries.yu@ringcentral.com>
2023-10-17 10:07:36 +0800

    test: update process-util.test.ts

--- test/process-util.test.ts
+++ test/process-util.test.ts
@@ -1,10 +1,33 @@
 import { ProcessUtil } from "../src";
+import { exec } from "child_process";
 
 it('process util test', async function () {
-    let stdout = await ProcessUtil.exec(`curl -s -o /Users/jeffries.yu/Downloads/rc.pkg https://electron.fiji.gliprc.com/downloads-all/stage/23.2.20/rc/7237-stage-23-2-20-noupdate-52d7d2d6a/for-downloading/RingCentral-23.2.20-7237-noupdate-mac-x64.pkg`);
+    let stdout = await ProcessUtil.exec(`pkill RingCentral XXX`);
     console.log(stdout);
 }, 400e3);
 
 it('install sf', async function () {
     await ProcessUtil.exec(`sudo npm install -g selenium-federation-server --registry=http://nexus3-xmn02.lab.nordigy.ru/repository/npm-group/`);
 }, 400e3);
+
+export class AutoCmdUtil {
+
+    public static async exec(script: string, timeout: number): Promise<{ stdout: string, stderr: string }> {
+        return await new Promise<{ stdout: string, stderr: string }>(function (resolve, reject) {
+            // maxBuffer is specified to avoid ERR_CHILD_PROCESS_STDIO_MAXBUFFER
+            exec(script, {
+                    maxBuffer: 10 * 1024 * 1024,
+                    timeout: timeout ? timeout : 70e3
+                },
+                (err, stdout: string, stderr: string) => {
+                    if (err) throw err;
+                    resolve({ stdout, stderr });
+                });
+        })
+    }
+
+}
+
+it('autocmd exec', async function () {
+    await AutoCmdUtil.exec(`pkill RingCentral`, 30e3);
+});
