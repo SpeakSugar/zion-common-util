b20b6afd8f1250b97a5c40a63d66f96838cb897c
jeffries.yu <jeffries.yu@ringcentral.com>
2024-03-14 09:40:55 +0800

    feat: add windowsHide: true for exec

--- src/process-util.ts
+++ src/process-util.ts
@@ -1,10 +1,12 @@
 import * as util from 'util';
 import { exec, ExecOptions } from "child_process";
 import { ProcessUtilException } from "./exception/process-util-exception";
+import * as _ from "lodash";
 
 export class ProcessUtil {
 
-    public static async exec(command: string, options?: ExecOptions): Promise<string> {
+    public static async exec(command: string, options: ExecOptions = {}): Promise<string> {
+        options = _.merge(options, { windowsHide: true });
         let p_exec = util.promisify(exec);
         try {
             let { stdout, stderr } = options ? await p_exec(command, options) : await p_exec(command);
