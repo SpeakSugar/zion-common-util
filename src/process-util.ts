87a17a98e648153c6d3bee1839bafbdc1ed62ae9
jeffries.yu <jeffries.yu@ringcentral.com>
2024-08-20 10:50:31 +0800

    fix: optimize for vite

--- src/process-util.ts
+++ src/process-util.ts
@@ -1,13 +1,13 @@
 import * as util from 'util';
-import { exec, ExecOptions } from "child_process";
+import * as child_process from "child_process";
 import { ProcessUtilException } from "./exception/process-util-exception";
 import * as _ from "lodash";
 
 export class ProcessUtil {
 
-    public static async exec(command: string, options: ExecOptions = {}): Promise<string> {
+    public static async exec(command: string, options: child_process.ExecOptions = {}): Promise<string> {
         options = _.merge(options, { windowsHide: true });
-        let p_exec = util.promisify(exec);
+        let p_exec = util.promisify(child_process.exec);
         try {
             let { stdout, stderr } = options ? await p_exec(command, options) : await p_exec(command);
             if (stderr) {
