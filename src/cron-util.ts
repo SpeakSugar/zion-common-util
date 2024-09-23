3a3542e8d5013fb47ec898765355434dc69d17f0
jeffries.yu <jeffries.yu@ringcentral.com>
2024-09-23 11:32:11 +0800

    refactor: node-cron => cron

--- src/cron-util.ts
+++ src/cron-util.ts
@@ -1,11 +1,18 @@
-import * as cron from "node-cron";
+import { CronJob } from "cron";
 
 export class CronUtil {
 
-    private static tasks: cron.ScheduledTask[] = [];
+    private static tasks: CronJob[] = [];
 
     public static schedule(cronExpression: string, callback: () => void) {
-        const task = cron.schedule(cronExpression, callback);
+        const task = new CronJob(
+            cronExpression,
+            callback,
+            null,
+            true,
+            'Asia/Shanghai'
+        )
+
         this.tasks.push(task);
         return task;
     }
