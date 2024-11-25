d4055c8293f8e15db7ca29b501521e889ca819ac
jeffries.yu <jeffries.yu@ringcentral.com>
2024-11-25 15:22:14 +0800

    test: add flag-util test

--- test/flag-util.test.ts
+++ test/flag-util.test.ts
@@ -4,3 +4,18 @@ it(`flag util test`, async () => {
     await FlagUtil.setFlag(`test`, `zzzz`, `false`);
     console.log(await FlagUtil.getFlag(`test`, `zzzz`));
 });
+
+it(`flag util obj`, async () => {
+    let obj = [{
+        name: "xxxx",
+        age: 12
+    }];
+    let obj2 = [{
+        name: "yyyy",
+        age: 17
+    }]
+    await FlagUtil.setFlag(`obj-test`, `obj`, JSON.stringify(obj));
+    await FlagUtil.setFlag(`obj-test`, `obj`, JSON.stringify(obj2));
+    let value = await FlagUtil.getFlag(`obj-test`, `obj`);
+    console.log(JSON.parse(value)[0].name);
+});
